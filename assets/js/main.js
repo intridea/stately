0ae00465a903801ffb0471f643ea44d7b0308199
CYHung <cherihung@gmail.com>
2013-07-03 18:41:58 -0400

    *required changes to be fully d3-powered
    js function to generate map, sass to style, and demo html file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,39 @@
 /* Use D3 to dynamically create a stately map
- * Created: @cyhung June 2013
+ * Created: @cyhung July 2013
+ * version 1.0
   */
-function drawMap(data) {
-
-var colorRange = ["#6BAED6","#3182BD","#08519C","#073E75"]; //d3 color hex
-    
-var color = d3.scale.ordinal()
-.domain([40, 50, 60, 70])
-.range(colorRange);
-  var nodes = classes(data);
+function drawMap() {
+  
+  drawMap.prototype.draw = function(mapid, datasource, datacol) {
+  //load data from csv url
+  d3.csv(datasource, function(data) {
+  
+  //nest data from csv into useful objects
+  var nodes = d3.nest()
+  .entries(data);
+  
+   //loop to build map
    for (var i=0;i<1;i++){
-    d3.select("ul#mymap")
-        .selectAll("ul#mymap li")
+    d3.select('#'+mapid)
+        .selectAll('#'+mapid+' li')
         .data(nodes)
         .enter()
         .append("li")
-            .attr("class",function(d){return d.abb.trim()})
-            .style("color", function(d){return color(d.value)})
-            .attr("data-state",function(d){return d.abb.trim()})
-            .text(function(d){return d.letter});
+            .attr("id",function(d){return d.abb}) //add id to state abbreviation
+            .attr("class",function(d){return toClass(d[datacol]);}) //add class to value in data column, this determines the color of each state
+            .attr("data-state",function(d){return d.abb}) //add data-state attr to state abbreviation
+            .text(function(d){return d.letter}); //add letter corresponding to each state. this is a fail-safe for older browsers
     }
- //translate bubble nodes from csv data
-function classes(nodes) {
- var vizdata = [];
+    
+    });
   
-    //Lazily compute children.
-    nodes.forEach(function(d) {
-    vizdata.push({
-    letter: d.letter,
-    abb: d.abb,
-    state: d.fullname,
-    value: d.value});
-   });
-   return vizdata;
-}
-
-
-
+  }; //end draw();
+  
+  //converts data value to usable class; strips all white spaces and lower case e.g. I am data value => iamdatavalue
+  function toClass(d) {
+    var classname = d.replace(/\s/g, "").toLowerCase();
+    return classname;
+  }
+  
 }; //end drawmap();
 
